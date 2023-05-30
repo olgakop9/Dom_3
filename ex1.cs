1e73aedfb347e854983a1f0824d2605a1aa4c409
Olga <Olga_kopylova9@mail.ru>
2023-05-31 00:12:16 +0500

    Задача_19

---
+++ ex1.cs
@@ -0,0 +1,30 @@
+//Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void palindrom()
+{
+    int number = new Random().Next(1, 100000); Для случайных чисел
+    Console.Write("Insert 5-digit number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int num1 = number / 10000 % 10;
+    int num2 = number / 1000 % 10;
+    int rev1 = number / 10 % 10;
+    int rev2 = number % 10;
+    int nul = number / 10000;
+
+    if (nul < 1 || nul > 9)
+    {
+        Console.WriteLine(number + " -> " + "Not 5-digit number");
+    }
+    else if (num1 == rev2 && num2 == rev1)
+    {
+        Console.WriteLine(number + " -> Yes, this is palindrom");
+    }
+    else
+    {
+        Console.WriteLine(number + " -> No, this is not palindrom");
+    }
+}
+for (int i = 0; i < 10; i++)
+{
+    palindrom();
+}
